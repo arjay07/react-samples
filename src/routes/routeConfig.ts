5c2b16a847616cfe84a63327e6a1a27420c4f55a
Leandro Yabut <arjay.07.ly@gmail.com>
2025-04-24 15:34:51 -0600

    Add ImageCarouselII component and view, update route configuration

--- src/routes/routeConfig.ts
+++ src/routes/routeConfig.ts
@@ -1,4 +1,5 @@
 import ImageCarouselView from "../views/ImageCarouselView";
+import ImageCarouselViewII from "../views/ImageCarouselViewII";
 
 export type RouteConfig = {
     label: string;
@@ -11,5 +12,10 @@ export const routes: RouteConfig[] = [
         label: "Image Carousel",
         path: "/image-carousel",
         component: ImageCarouselView,
+    },
+    {
+        label: "Image Carousel II",
+        path: "/image-carousel-ii",
+        component: ImageCarouselViewII,
     }
 ].sort((a, b) => a.label.localeCompare(b.label));
---
+++ src/views/ImageCarouselViewII.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ImageCarousel from "../components/ImageCarousel";
+
+const images = [
+    {
+      src: "https://picsum.photos/id/600/600/400",
+      alt: "Forest",
+    },
+    {
+      src: "https://picsum.photos/id/100/600/400",
+      alt: "Beach",
+    },
+    {
+      src: "https://picsum.photos/id/200/600/400",
+      alt: "Yak",
+    },
+    {
+      src: "https://picsum.photos/id/300/600/400",
+      alt: "Hay",
+    },
+    {
+      src: "https://picsum.photos/id/400/600/400",
+      alt: "Plants",
+    },
+    {
+      src: "https://picsum.photos/id/500/600/400",
+      alt: "Building",
+    },
+  ];
+const ImageCarouselView: React.FC = () => {
+    return <>
+      <div className="referenceLink">
+        <a className="btnLink" href="https://www.greatfrontend.com/interviews/study/amazon/questions/user-interface/image-carousel-ii/react" target="_blank" rel="noopener noreferrer">
+          Open Challenge on GreatFrontEnd: <span className="fw-bold">Image Carousel II</span>
+        </a>
+      </div>
+      <ImageCarousel images={images} />
+    </>;
+};
+
+export default ImageCarouselView;
